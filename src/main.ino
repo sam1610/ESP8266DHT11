0d5ec99c9eef146e56b9bff2d13a4e1efb14c827
sam1610 <samir.amri@gmai.com>
2019-01-17 22:47:55 +0300

    delay added

---
+++ src/main.ino
@@ -0,0 +1,34 @@
+#include <Arduino.h>
+#include <DHT.h>
+#include <Adafruit_Sensor.h>
+#define DHTPIN D2
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+void setup(){
+  // initialiser la vitesse de communication serie
+    Serial.begin(9600);
+    DHTstart();
+    }
+void loop(){
+  Serial.print("Temperature = "); 
+  Serial.print(ReadT());
+  Serial.println(" C");
+  Serial.print("Humidity = "); 
+  Serial.print(ReadH());
+  Serial.println(" %");
+  delay(3000);
+}
+
+void DHTstart(){
+  dht.begin();
+}
+
+float ReadT(){
+  float t = dht.readTemperature();
+  return t;
+}
+
+float ReadH(){
+  float h = dht.readHumidity();
+  return h;
+}
