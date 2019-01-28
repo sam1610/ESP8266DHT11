b397805807d081207ba5a81e3ec376f0a0e846ec
sam1610 <samir.amri@gmail.com>
2019-01-28 22:04:20 +0300

    initialization

--- src/main.ino
+++ src/main.ino
@@ -1,21 +1,3 @@
-#include <Arduino.h>
-#include <DHT.h>
-#include <Adafruit_Sensor.h>
-#define DHTPIN D2
-#define DHTTYPE DHT11
-DHT dht(DHTPIN, DHTTYPE);
-void setup(){
-    Serial.begin(9600);
-    DHTstart();
-    }
-void loop(){
-  Serial.print("Temperature = "); 
-  Serial.print(ReadT());
-  Serial.println(" C");
-  Serial.print("Humidity = "); 
-  Serial.print(ReadH());
-  Serial.println(" %");
-}
 
 
 
